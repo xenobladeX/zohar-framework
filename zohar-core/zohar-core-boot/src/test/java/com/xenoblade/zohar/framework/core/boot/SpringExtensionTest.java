baac853c3da51b713d0bf65f587750fcd3941dca
丁明 <dingming@cmhi.chinamobile.com>
2022-01-30 11:12:57 +0800

    change compiler config of zohar-core-extension

--- zohar-core/zohar-core-boot/src/test/java/com/xenoblade/zohar/framework/core/boot/SpringExtensionTest.java
+++ zohar-core/zohar-core-boot/src/test/java/com/xenoblade/zohar/framework/core/boot/SpringExtensionTest.java
@@ -15,14 +15,17 @@
  */
 package com.xenoblade.zohar.framework.core.boot;
 
+import com.xenoblade.zohar.framework.core.api.dto.BasicErrorCode;
+import com.xenoblade.zohar.framework.core.api.dto.IZoharErrorCode;
 import com.xenoblade.zohar.framework.core.boot.extension.ITestExtension;
+import com.xenoblade.zohar.framework.core.boot.extension.TestErrorCode;
+import com.xenoblade.zohar.framework.core.extension.enumeration.ZoharEnumFactory;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
-import java.util.List;
 
 /**
  * SpringExtensionTest
@@ -40,6 +43,14 @@ public class SpringExtensionTest {
     @Test
     public void testSpringExtension() {
         Assertions.assertEquals(2, testExtensions.size());
+    }
+
+    @Test
+    public void testExtensionEnum() {
+        IZoharErrorCode errorCode = ZoharEnumFactory.INSTANCE.valueOf(100000, IZoharErrorCode.class);
+        Assertions.assertEquals(BasicErrorCode.OK, errorCode);
+        errorCode = ZoharEnumFactory.INSTANCE.valueOf(666, IZoharErrorCode.class);
+        Assertions.assertEquals(TestErrorCode.TEST_ERROR, errorCode);
     }
 
 }
